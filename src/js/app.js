c94e75f597ae05562601be6971fec1ea1dd8b72f
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-03-28 09:20:54 -0600

    Improve the application design separating concerns

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,51 @@
-
-// bind a new instance of the viewModel to the page
-ko.applyBindings(ViewModel);
-
-
 // Initialize google map
 function initialize() {
         var mapOptions = {
           center: { lat: 10.3008886, lng: -85.8380699},
-          zoom: 15
+          zoom: 15,
+
         };
         var map = new google.maps.Map(document.getElementById('map'),
             mapOptions);
+
+        // custom style to hide the default interest points of the map
+        var noPoi = [
+            {
+                featureType: "poi",
+                stylers: [
+                    { visibility: "off" }
+                ]   
+            }
+        ];
+
+        // set the style to the map
+        map.setOptions({styles: noPoi});
+
+        //initialize saved makers
+        var makers = [];
+
+        // create the ViewModel with the created makers and current map
+        var viewModel = new ViewModel(makers, map);
+        // bind a new instance of the viewModel to the page
+        ko.applyBindings(viewModel);        
+
+        // send all map events to the knockout function
+        google.maps.event.addListener(map, "click", function(event) {
+            viewModel.mapClick(map, event);
+        });
 }
 
 // attach the initialize to the page load
 google.maps.event.addDomListener(window, 'load', initialize);
+
+// add maker to the map
+function addMakers(map)
+{
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(10.3008886,-85.8380699),
+      map: map,
+      title: 'Hello World!'
+    });    
+}
+
+
--- src/js/viewmodel.js
+++ src/js/viewmodel.js
@@ -1,8 +1,39 @@
 // the main ViewModel
-var ViewModel = function( makers ) {
+var ViewModel = function( makers, map ) {
     makers = makers || [];
+    
+    //reference of the google map object
+    this.map = map;
+
     // map the makers, and create an observable array with them
     this.makers = ko.observableArray( makers.map( function ( maker ) {
        return new Maker();
     } ) );
+
+    // initialize the current maker before being saved into the model
+    this.currentMaker = ko.observable();
+
+    // add a new maker to the observable array.
+    this.saveMaker = function() {
+        var maker = this.currentMaker();
+    };
+
+    // manage the map clicks
+    this.mapClick = function( map, event ) {
+        var lat = event.latLng.lat();
+        var lng = event.latLng.lng();
+
+        console.log( "Lat=" + lat + "; Lng=" + lng );
+    };
+
+    // manage the markers clicks
+    this.markerClick = function( marker, event ) {
+
+    };
+
+    // print information into the console log
+    this.writeLog = function(text) {
+        console.log(text);
+    };
+
 }
