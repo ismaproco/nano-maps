bdb13cf69b9dd4c84710242bc328c9d948d9e4ad
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-03-28 09:19:32 -0600

    Added custom attributes to the model marker

--- src/js/models/marker.js
+++ src/js/models/marker.js
@@ -1,6 +1,8 @@
-function Maker( name, lat, lon )
+function Marker( name, lat, lon )
 {
     this.name = name;
     this.lat = lat;
     this.lon = lon;
+    this.metaTags = [];
+    this.links = [];
 }
