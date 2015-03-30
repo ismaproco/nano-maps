4f9126b725192ec1c8d748c08173a52405f6a1a3
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-03-30 15:39:17 -0600

    Added view objects to the application

---
+++ src/js/views/markerTypes.js
@@ -0,0 +1,22 @@
+/* Starting Marker Types */
+
+var markerTypes = [
+    new MarkerType ( 
+        { type:'restaurant', url:'images/Apple.png', title: 'Restaurant' } 
+    ),
+    new MarkerType ( 
+        { type:'bar', url:'images/Beer.png', title: 'Bar' } 
+    ),
+    new MarkerType ( 
+        { type:'coffe', url:'images/Coffecan.png', title: 'Coffe' } 
+    ),
+    new MarkerType ( 
+        { type:'diving', url:'images/Diving.png', title: 'Diving' } 
+    ),
+    new MarkerType ( 
+        { type:'romance', url:'images/Hearts.png', title: 'Romance' } 
+    ),
+    new MarkerType ( 
+        { type:'movies', url:'images/movierollsmall.png', title: 'Movies' } 
+    ),
+];
